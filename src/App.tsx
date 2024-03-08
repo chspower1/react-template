841ec20155e80255b5cd8f200dec8d7b623ae523
hosung cho <chspower1@gmail.com>
2024-03-08 13:28:46 +0900

    setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient();
