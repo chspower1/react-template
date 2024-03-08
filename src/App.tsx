a7e8ece514149666f457f0000a67b576d204fda5
hosung cho <chspower1@gmail.com>
2024-03-08 13:26:47 +0900

    setup react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const queryClient = new QueryClient();
 
 const App = () => {
-  return <div className="App">frontend template</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <div className="App">frontend template</div>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
